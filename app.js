29fb00834b433e9390500035adbdf42b9a20c264
elasheras <emperorbfd@gmail.com>
2024-02-29 19:42:28 -0300

    {{}} Added handlebars js 📂Added public assets folder 👁 Added a few views and controller for frontend 🎨Added Sweet alerts tag in the main.handlebars

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import dotenv from 'dotenv';
 import express from "express";
+// __dirname of the project
+import __dirname from './utils.js';
+// Handlebars
+import handlebars from 'express-handlebars';
+// Routes
 import baseAppRouter from "./src/routes/base.js";
 import productsRouter from "./src/routes/ProductRouter.js";
 import cartsRouter from "./src/routes/CartsRouter.js";
+import userRouter from "./src/routes/UserRouter.js";
 
 
 /** CONSTANTS **/
@@ -10,17 +16,35 @@ const ANSImagenta = '\x1b[35m%s\x1b[0m';
 const ANSIgreen =   '\x1b[32m%s\x1b[0m';
 const ANSIred =     '\x1b[31m%s\x1b[0m';
 
+
 /** VAR **/
 dotenv.config();
+// Express App Instance
 const app = express();
+// Port
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
-
+// Static Assets (public folder)
+app.use(express.static(__dirname+'/src/public'));
+// Parse req bodies to JSON automatically
 app.use(express.json()); // This line is crucial for parsing JSON req bodies
+
 
 // Routes
 app.use('/', baseAppRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+app.use('/users', userRouter);
+
+
+// Handlebars setup
+// Initialize the handlebars engine
+app.engine('handlebars', handlebars.engine());
+// Set the project views path
+console.log(__dirname+'/src/views');
+app.set('views', __dirname+'/src/views');
+// Set the view engine as handlebars
+app.set('view engine', 'handlebars');
+
 
 // Start server
 app.listen(SERVER_PORT, () => {
---
+++ src/routes/UserRouter.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const router = express.Router();
+
+// GET
+
+/**
+ * recieves a query id and returns a SINGLE product
+ */
+router.get('', async (req, res) => {
+
+    res.render('index', {
+        name: 'Jane Doe'
+    });
+});
+
+export default router;
